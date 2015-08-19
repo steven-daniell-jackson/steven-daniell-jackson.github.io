b06f0980329d56d3bb9052e13c167e090fc6ee3f
Steven Jackson <stevenjackson.sanguine@gmail.com>
2015-08-19 19:27:38 +0200

    additional header styling and jq trigger added for about me

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+$(function(){
 
+
+
+// Add event listener "More about me" CTA
+$('a[data-about-me="more"]').on("click", function(){
+	$('.more-about-me').slideToggle('400', function() {
+
+
+$('a[data-about-me="more"]').text("Less about me? Are you sure?");;
+
+
+
+
+
+	});
+
+
+});
+
+
+
+})
