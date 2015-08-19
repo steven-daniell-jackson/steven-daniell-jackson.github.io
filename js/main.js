d11549b74085c797fe2fff81a960e5905fc962ad
Steven Jackson <stevenjackson.sanguine@gmail.com>
2015-08-19 20:26:47 +0200

    About me jq triggers functional

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
 $(function(){
 
+/**************************************************************************
 
+EVEMT LISTENER - ABOUT ME 
 
-// Add event listener "More about me" CTA
+**************************************************************************/ 
+
+// Add event listener "More about me" 
 $('a[data-about-me="more"]').on("click", function(){
-	$('.more-about-me').slideToggle('400', function() {
+
+	// Change Background color of arrow before Event
+	$('div.container.arrow-about').css("background-color", "#292929");
+
+	// Animate div down
+	$('.more-about-me').slideDown('400', function() {
+		$('a[data-about-me="more"]').css("opacity", "0");
 
 
-$('a[data-about-me="more"]').text("Less about me? Are you sure?");;
+	}); 
+}); //End "More about me" Trigger
 
 
+// Add event listener "Less about me" 
+$('a[data-about-me="less"]').on("click", function(){
+
+// Animate div up
+$('.more-about-me').slideUp('400', function() {
+
+	// Change Background color of arrow during Event
+	$('div.container.arrow-about').css("background-color", "#fff");
+	$('a[data-about-me="more"]').css("opacity", "1");
+	
+
+}); 
+}); //End "Less about me" Trigger
 
 
+/**************************************************************************
 
-	});
+END EVEMT LISTENER - ABOUT ME 
+
+**************************************************************************/ 
 
 
-});
-
-
-
-})
+}); //Document Ready
